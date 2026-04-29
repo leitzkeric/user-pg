b4bb8bab6cf69b265871c7fb1a5ea8050531ba7c
leitzkeric <ricardo.leitzke@gmail.com>
2026-04-29 15:52:10 -0300

    Configura a API para STATELESS

---
+++ src/main/java/com/trooper/user_pg/infra/security/SecurityConfigurations.java
@@ -0,0 +1,21 @@
+package com.trooper.user_pg.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
